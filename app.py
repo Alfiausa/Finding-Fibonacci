b198612b3d7ec08e52f6533331183ea8f16e79f1
Alfia <alfiausa7@gmail.com>
2018-10-22 23:52:47 -0400

    add workflow from page 1 to 5 with Painterro screen for drawing

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, json, request, redirect, url_for
+from flask import Flask, render_template, json, request, redirect, url_for, jsonify
 
 import datetime
+import os
+import base64
+from time import time
+import tempfile
 
 app = Flask(__name__)
 
@@ -10,7 +14,7 @@ password = 'none'
 @app.route('/about',  methods=['GET', 'POST'])
 def about():
 
-    return render_template('about.html')
+    return render_template('ShellLearn_About.html')
 
 @app.route("/")
 def main():
@@ -29,18 +33,30 @@ def login():
             return redirect(url_for('about'))
     return render_template('login.html', error=error)
 
-@app.route('/level1ABC', methods=['GET', 'POST'])
+@app.route('/ShellLearn_book', methods=['GET', 'POST'])
 def level1ABC():
-    if userID != 'none' :
+    # if userID != 'none' :
 
-        return render_template('*****.html')
-    else:
-       return render_template('login.html') 
+        return render_template('ShellLearn_book.html')
+    # else:
+    #    return render_template('login.html') 
 
 @app.route('/home',  methods=['GET', 'POST'])
 def home():
 
     return render_template('index.html')
+
+TMP_DIR_NAME = 'Images'
+
+@app.route("/save-as-base64/", methods=['POST'])
+def base64_saver():
+    filename = '{:10d}.png'.format(int(time()))  # generate some filename
+    filepath = os.path.join(TMP_DIR_NAME, filename)
+    with open(filepath, "wb") as fh:
+        base64_data = request.json['image'].replace('data:image/png;base64,', '')
+        fh.write(base64.b64decode(base64_data))
+
+    return jsonify({})
 
 
 if __name__ == "__main__":
